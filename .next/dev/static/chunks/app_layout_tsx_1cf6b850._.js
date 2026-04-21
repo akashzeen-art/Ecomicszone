e49a5b7d0f6072930306bcb618c8efd2a254afb4
akashzeen-art <akashzeen@gmail.com>
2026-04-21 14:10:02 +0530

    Remove node_modules, add .gitignore

--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_77026a5b._.css",
-  "static/chunks/node_modules_2512ec61._.js",
-  "static/chunks/_c065c761._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/components_home_fan-art-showcase_tsx_5079f4af._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_97790c1a._.js",
-  "static/chunks/_d92f2e61._.js"
-],
-    source: "dynamic"
-});
